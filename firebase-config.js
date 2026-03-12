a70ea51a262c143eba708a9eb57bd675ad6e253f
Fatema <fsalim@squ.edu.om>
2026-03-12 22:23:30 +0400

    تصحيح storageBucket في إعدادات Firebase ليعمل مع قاعدة البيانات والمرفقات بشكل صحيح

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDmNC9-8sRAHGqGSC9r_Zr3mk97tu3RFgc",
   authDomain: "squh-training.firebaseapp.com",
   projectId: "squh-training",
-  storageBucket: "squh-training.firebasestorage.app",
+  storageBucket: "squh-training.appspot.com",
   messagingSenderId: "1064112237940",
   appId: "1:1064112237940:web:94905f060413b97ad6d021",
   measurementId: "G-NNKT4PCV5T"
